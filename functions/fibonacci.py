057ad7bb1312f860a1f161e1f86d95d489b24024
Fasih Khatib <hellofasih.confound928@passinbox.com>
2024-11-21 19:17:34 +0530

    Handle the case of n < 0
    Raise an assertion error when n < 0.
    Split test cases into two for handling positive and negative values.

--- functions/fibonacci.py
+++ functions/fibonacci.py
@@ -6,6 +6,8 @@ def fibonacci(n: int) -> int:
     """
     Computes the nth number in the Fibonacci sequence.
     """
+    assert n >= 0, f"n must be >= 0; {n} was passed."
+
     if n <= 1:
         return n
 
--- test/functions/test_fibonacci.py
+++ test/functions/test_fibonacci.py
@@ -1,8 +1,15 @@
-from hypothesis import given, strategies as st
+import pytest
+from hypothesis import given, strategies as st, example
 
 from functions import fibonacci
 
 
-@given(st.integers())
+@given(st.integers(min_value=0, max_value=10))
 def test_fibonacci(n):
     assert fibonacci(n) >= 0
+
+
+@given(st.integers(min_value=-2, max_value=-1))
+def test_fibonacci(n):
+    with pytest.raises(AssertionError):
+        fibonacci(n)
