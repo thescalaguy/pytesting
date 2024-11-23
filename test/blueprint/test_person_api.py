3b6a24a6db1d0fca3c46200bcaaac766c08a833a
Fasih Khatib <hellofasih.confound928@passinbox.com>
2024-11-23 11:30:06 +0530

    Add a property test for /person API.

--- test/blueprint/test_person_api.py
+++ test/blueprint/test_person_api.py
@@ -1,7 +1,25 @@
-def test_create_person(test_client):
+from hypothesis import given, strategies as st
+import datetime
+
+from model.person import Person
+
+MIN_DATE = datetime.date.today() - datetime.timedelta(days=Person.MAX_AGE * 365)
+MAX_DATE = datetime.date.today()
+
+
+@given(
+    json=st.fixed_dictionaries(
+        {
+            "name": st.text(min_size=5),
+            "dob": st.dates(min_value=MIN_DATE, max_value=MAX_DATE),
+        }
+    )
+)
+def test_create_person(json, test_client):
     response = test_client.post(
         "/api/person",
-        json={"name": "test", "dob": "2024-01-01"},
+        json=json,
         headers={"Content-Type": "application/json"},
     )
+
     assert response.status_code == 200
